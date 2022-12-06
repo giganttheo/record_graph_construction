f94e4ad002a64998ab8863907b07b09700b3260b
theo.gigant <giganttheo.com>
2022-12-06 17:01:19 +0100

    dl script

--- dl_and_process.py
+++ dl_and_process.py
@@ -7,15 +7,15 @@ from process_video import process_video
 
 
 if __name__ == "__main__":
-    with open("./dl_status.json", "r") as f:
+    with open("./record_graph_construction/dl_status.json", "r") as f:
         dl_status = json.load(f)
-    with open("./dl_status_local.json", "r") as f:
+    with open("./record_graph_construction/dl_status_local.json", "r") as f:
         dl_status_local = json.load(f)
     for k in tqdm(dl_status.keys()):
         if not dl_status[k] and not dl_status_local[k]:
-            os.system(f"wget -O ../videos/ {k}")
+            os.system(f"wget -O ./videos/ {k}")
             dl_status_local[k] = True
             with open("./dl_status_local.json", "w") as f:
                 f.dump(dl_status_local, f)
             process_video(k.split("/")[-1], None, False)
-            os.system(f"rm ../videos/{k.split('/')[-1]}")
+            os.system(f"rm ./videos/{k.split('/')[-1]}")
