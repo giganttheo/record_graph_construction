106dbb05e4ce73ed0a9a2cfa81b255f00ea7a62c
theo.gigant <giganttheo.com>
2022-12-06 16:59:05 +0100

    dl script

--- dl_and_process.py
+++ dl_and_process.py
@@ -2,11 +2,20 @@ from tqdm import tqdm
 import json
 import os
 
+from file_check import check_folder
+from process_video import process_video
+
 
 if __name__ == "__main__":
     with open("./dl_status.json", "r") as f:
         dl_status = json.load(f)
-    for k in dl_status.keys():
-        if not dl_status[k]:
-            os.system(f"wget {k}")
-            break
+    with open("./dl_status_local.json", "r") as f:
+        dl_status_local = json.load(f)
+    for k in tqdm(dl_status.keys()):
+        if not dl_status[k] and not dl_status_local[k]:
+            os.system(f"wget -O ../videos/ {k}")
+            dl_status_local[k] = True
+            with open("./dl_status_local.json", "w") as f:
+                f.dump(dl_status_local, f)
+            process_video(k.split("/")[-1], None, False)
+            os.system(f"rm ../videos/{k.split('/')[-1]}")
