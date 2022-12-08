a77edaac28c5037159c4bf0bde7339c8163331f5
theo.gigant <giganttheo.com>
2022-12-08 15:43:59 +0100

    fixed dl and process script

--- dl_and_process.py
+++ dl_and_process.py
@@ -39,35 +39,32 @@ def process_video(transcription_model, vid_file):
             err_slides.append(vid_path)
     transc_path = f'{FOLDER_PATH}/transcripts/{".".join(vid_file.split(".")[:-1])}.json'
     if os.path.isfile(aud_path) and not os.path.isfile(transc_path):
-        #try:
-        result = transcribe(transcription_model, aud_path)
-        with open(transc_path, "w") as f:
-            f.write(json.dumps(result))
-        # except:
-        #     err_transcription.append(aud_path)
+        try:
+            result = transcribe(transcription_model, aud_path)
+            with open(transc_path, "w") as f:
+                f.write(json.dumps(result))
+        except:
+            err_transcription.append(aud_path)
     else:
         err_transcription.append(aud_path)
     return err_audio, err_slides, err_transcription
 
 if __name__ == "__main__":
     transcription_model = whisper.load_model("small")
-    for vid_file in sorted(os.listdir(SOURCE_PATH)):
-        print(process_video(transcription_model, vid_file))
-    # with open("./record_graph_construction/dl_status.json", "r") as f:
-    #     dl_status = json.load(f)
-    # with open("./record_graph_construction/dl_status_local.json", "r") as f:
-    #     dl_status_local = json.load(f)
-    # for k in dl_status.keys():
-    #     print(k)
-    #     if not dl_status[k] and not dl_status_local[k]:
-    #         os.system(f"wget -O ./videos/ {k}")
-    #         dl_status_local[k] = True
-    #         with open("./dl_status_local.json", "w") as f:
-    #             f.dump(dl_status_local, f)
-    #         err_audio, err_slides, err_transcription = process_video(k.split("/")[-1], None, False)
-    #         if (len(*err_audio, *err_slides, *err_transcription)) > 0:
-    #             print(err_audio, err_slides, err_transcription)
-    #         dl_status_local[k] = True
-    #         with open("./record_graph_construction/dl_status_local.json", "w") as f:
-    #             json.dump(dl_status_local, f)
-    #         os.system(f"rm ./videos/{k.split('/')[-1]}")
+    with open("./record_graph_construction/dl_status.json", "r") as f:
+        dl_status = json.load(f)
+    with open("./record_graph_construction/dl_status_local.json", "r") as f:
+        dl_status_local = json.load(f)
+    for k in dl_status.keys():
+        print(k, end=" ... ")
+        if not dl_status[k] and not dl_status_local[k]:
+            os.system(f"wget -O ./videos/ {k}")
+            with open("./dl_status_local.json", "w") as f:
+                f.dump(dl_status_local, f)
+            err_audio, err_slides, err_transcription = process_video(transcription_model, k.split("/")[-1])
+            if (len(*err_audio, *err_slides, *err_transcription)) > 0:
+                print("error")
+                #print(err_audio, err_slides, err_transcription)
+            else:
+                print("ok")
+            os.system(f"rm ./videos/{k.split('/')[-1]}")
